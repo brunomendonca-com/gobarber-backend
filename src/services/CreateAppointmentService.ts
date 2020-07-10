38be0f086c0499336671cb63555f4aff8b1dfa72
Bruno Mendonca <me@brunomendonca.com>
2020-07-09 19:30:34 -0600

    Add Exception Handling

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -1,6 +1,8 @@
 import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 
+import AppError from '../errors/AppError';
+
 import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
@@ -23,7 +25,7 @@ class CreateAppointmentService {
     );
 
     if (findAppointmentOnSameDate) {
-      throw Error('This date and time are already booked.');
+      throw new AppError('This date and time are already booked.');
     }
 
     const appointment = appointmentsRepository.create({
